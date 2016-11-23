55173a2a61523a93bfd0dbed2275f33460bed7aa
JGH85 <josephghouser@gmail.com>
2016-11-23 13:57:52 -0600

    first commit latingenie with dependencies

---
+++ src/main/java/com/latingenie/LatingenieApplication.java
@@ -0,0 +1,12 @@
+package com.latingenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatingenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatingenieApplication.class, args);
+	}
+}
